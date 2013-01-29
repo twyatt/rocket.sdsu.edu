840edcfe9fbebef2c45f6c0305e4abbb2bc5b4f8
Travis Wyatt <travis.i.wyatt@gmail.com>
2013-01-28 21:12:01 -0800

    Updated about page with text Jen provided.

--- pages/about.php
+++ pages/about.php
@@ -5,7 +5,13 @@ $title = 'About';
 <div class="page">
 	<h1>About</h1>
 	
-	We are a club on San Diego State University Campus. As students we design, build, and launch a rocket with the assistance of our advisor. Every year we look to improve upon our past rockets and use industry leading technology. We currently have 40 active members who collaborate on different systems of our rocket.
+	The San Diego State University (SDSU) Rocket Project was originally founded in May 2003 and the
+original rocket team designed, fabricated, and launched 4 rockets. The initial design and fabrication of
+the Rocket Project's 5th rocket began in 2008, but was never fully completed. In fall of 2011, the Rocket
+Project was picked up once again by engineering students with the goal of finally completing the rocket.
+Within one year we expanded the Rocket Project from a team of 5 students to 45 students, culminating
+in the successful launch and recovery of our 18 foot, liquid fueled rocket. Take a look at our <a href="http://sdsurocketproject.blogspot.com/" target="_blank">blog</a> to
+follow the SDSU Rocket Project's current projects and the progress we have been making!
 	<br />
 	<br />
 	
