e8c051e1bbc8e8844abf7b6ace531ff4655cf602
Travis Wyatt <travis.i.wyatt@gmail.com>
2013-04-30 16:36:05 -0700

    Setup PayPal donate button on donate page.

--- pages/donate.php
+++ pages/donate.php
@@ -4,5 +4,22 @@ $title = 'Donate';
 
 <div class="page">
 	<h1>Donate</h2>
-	Coming soon.
+	
+<?php
+if ($_REQUEST['action'] == 'success') {
+?>
+	Thank you!
+<?php
+} else {
+?>
+	You can donate via PayPal using the button below:
+	<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+		<input type="hidden" name="cmd" value="_s-xclick">
+		<input type="hidden" name="hosted_button_id" value="AGXHLLTLQD24G">
+		<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+		<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+	</form>
+<?php
+}
+?>
 </div>
