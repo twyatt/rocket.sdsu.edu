7e5479c48acca6a6202b90230dba33b8b94b9963
Travis Wyatt <travis.i.wyatt@gmail.com>
2014-09-10 00:49:55 -0700

    Updated Members page to have an embedded Google Doc.

--- pages/members.php
+++ pages/members.php
@@ -2,7 +2,6 @@
 $title = 'Members';
 ?>
 
-<div class="page">
-	<h1>Members</h2>
-	Coming soon.
+<div class="page" style="height: 100%">
+	<iframe style="width: 920px; height: 100%" src="https://docs.google.com/document/d/1tvTU0rW_Pe_wG8w5wbkzpstKYRvXd7ICWkOA2aKpLP4/pub?embedded=true"></iframe>
 </div>
