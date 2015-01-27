a632ad75c717be6d945a0a2a9afa8893c70a5a5e
Travis Wyatt <travis.i.wyatt@gmail.com>
2015-01-26 23:45:50 -0800

    Updated sponsors page. Fixed members page embedded Google Doc.

--- pages/members.php
+++ pages/members.php
@@ -3,5 +3,5 @@ $title = 'Members';
 ?>
 
 <div class="page" style="height: 100%">
-	<iframe style="width: 920px; height: 100%" src="https://docs.google.com/document/d/1tvTU0rW_Pe_wG8w5wbkzpstKYRvXd7ICWkOA2aKpLP4/pub?embedded=true"></iframe>
+	<iframe style="width: 920px; min-height: 600px; height: 100%" src="https://docs.google.com/document/d/1tvTU0rW_Pe_wG8w5wbkzpstKYRvXd7ICWkOA2aKpLP4/pub?embedded=true"></iframe>
 </div>
--- pages/sponsors.php
+++ pages/sponsors.php
@@ -12,6 +12,12 @@ $title = 'Sponsors';
 		<a class="sponsor-link" href="http://www.flometrics.com/" target="_blank">
 			<div class="sponsor" style="background-image: url('images/flometrics.gif');"></div>
 		</a>
+		<a class="sponsor-link" href="http://www.wrotenlaw.com/" target="_blank">
+			<div class="sponsor" style="background-image: url('images/wa.jpg');"></div>
+		</a>
+		<a class="sponsor-link" href="http://www.asana.com" target="_blank">
+			<div class="sponsor" style="background-image: url('images/asana_logo.png');"></div>
+		</a>
 		<a class="sponsor-link" href="http://www.sdcomposites.com/" target="_blank">
 			<div class="sponsor" style="background-image: url('images/sdc.jpg');"></div>
 		</a>
@@ -21,14 +27,26 @@ $title = 'Sponsors';
 		<a class="sponsor-link" href="http://www.netburner.com/" target="_blank">
 			<div class="sponsor" style="background-image: url('images/netburner.jpg');"></div>
 		</a>
-		<a class="sponsor-link" href="http://www.wrotenlaw.com/" target="_blank">
-			<div class="sponsor" style="background-image: url('images/wa.jpg');"></div>
-		</a>
 		<a class="sponsor-link" href="http://www.goldenstatecellular.com/" target="_blank">
 			<div class="sponsor" style="background-image: url('images/golden_state_cellular_logo.png');"></div>
 		</a>
 		<a class="sponsor-link" href="http://www.embeddedadventures.com" target="_blank">
 			<div class="sponsor" style="padding-left: 65px; background-image: url('images/ea_logo.jpg');"></div>
+		</a>
+		<a class="sponsor-link" href="http://friendsofamateurrocketry.org" target="_blank">
+			<div class="sponsor" style="background-image: url('images/far_logo.png');"></div>
+		</a>
+		<a class="sponsor-link" href="http://www.virgingalactic.com" target="_blank">
+			<div class="sponsor" style="background-image: url('images/vg_logo.jpg');"></div>
+		</a>
+		<a class="sponsor-link" href="http://www.northropgrumman.com" target="_blank">
+			<div class="sponsor" style="background-image: url('images/ng_logo.png');"></div>
+		</a>
+		<a class="sponsor-link" href="http://mysolar.cat.com" target="_blank">
+			<div class="sponsor" style="background-image: url('images/st_logo.jpg');"></div>
+		</a>
+		<a class="sponsor-link" href="http://www.nasa.gov" target="_blank">
+			<div class="sponsor" style="background-image: url('images/nasa_logo.png');"></div>
 		</a>
 		<a class="sponsor-link" href="http://www.ni.com/academic" target="_blank">
 			<div class="sponsor" style="background-image: url('images/ni_logo.jpg');"></div>
